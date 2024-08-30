f5eaf7ee8007fdd796da3294e89e688cb9958315
adnrussg <russelgarcia1551@gmail.com>
2024-08-30 17:39:09 -0400

    Ternary Operator testing and sample

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -45,3 +45,8 @@ switch (userType){
 }
 
 console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
